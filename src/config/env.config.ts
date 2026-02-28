3dad59aba0650941b90c4f7763567cef3e61ed02
RashedAbdullah <maarashed40@gmail.com>
2026-02-28 17:35:10 +0600

    backend url updated

--- src/config/env.config.ts
+++ src/config/env.config.ts
@@ -1,5 +1,5 @@
 export const Environment = {
   API_URL: typeof window !== "undefined"
     ? "/api"
-    : process.env.NEXT_PUBLIC_API_BASE_URL || "http://localhost:3004/api",
+    : process.env.NEXT_PUBLIC_API_BASE_URL || "https://quickhire.dirasah.org/api",
 };
